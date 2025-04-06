cc9397728aa22a616e72717fe9903267bc1661ac
iammagash <74823831+Magashwarahan@users.noreply.github.com>
2025-04-06 19:59:53 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -301,4 +301,5 @@ def _corsify_actual_response(response):
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
